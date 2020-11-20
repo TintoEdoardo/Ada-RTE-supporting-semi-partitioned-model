2adbab07f228d44f5a569e57d2b3d26666b9b68f
Matia Bottaro <mattiabottaro9@gmail.com>
2020-11-20 11:20:19 +0100

    fixed script and removed experiment_data unit

--- build_runtime_zynq.sh
+++ build_runtime_zynq.sh
@@ -1,10 +1,10 @@
 
-mkdir ./cortex-ar/zynq7000/ravenscar-full/adalib/ ./cortex-ar/zynq7000/ravenscar-full/obj/ ./cortex-ar/zynq7000/ravenscar-full/user_srcs/ -p &&
+mkdir $PWD/cortex-ar/zynq7000/ravenscar-full/adalib/ $PWD/cortex-ar/zynq7000/ravenscar-full/obj/ $PWD/cortex-ar/zynq7000/ravenscar-full/user_srcs/ -p &&
 gprbuild -P ravenscar_full_zynq7000.gpr -j0 &&
 mkdir runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000 -p &&
 gprinstall -P ravenscar_full_zynq7000.gpr -p -f -XPREFIX=$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000 --install-name=ravenscar_full_zynq7000 &&
-[ -d "./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000" ] && mv ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000/* ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/;
-[ -d "./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000" ] && mv ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000/* ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/;
-[ -d "./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000" ] && mv ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000/* ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/;
+[ -d "$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000" ] && mv $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000/* $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/;
+[ -d "$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000" ] && mv $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000/* $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/;
+[ -d "$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000" ] && mv $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000/* $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/;
 
-rm -r ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000 ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000 ./runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000
+rm -rf $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000 $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000 $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnat/ravenscar_full_zynq7000
