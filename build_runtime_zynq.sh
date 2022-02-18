42ca5cd39d9e310bd7dcae1c48ddd5713762bb69
TintoEdoardo <tinto.edoardo@gmail.com>
2022-02-18 19:17:01 +0100

    Fixed the script for building

--- build_runtime_zynq.sh
+++ build_runtime_zynq.sh
@@ -1,7 +1,7 @@
 
-mkdir $PWD/cortex-ar/zynq7000/ravenscar-full/adalib/ $PWD/cortex-ar/zynq7000/ravenscar-full/obj/ $PWD/cortex-ar/zynq7000/ravenscar-full/user_srcs/ -p &&
+mkdir -p $PWD/cortex-ar/zynq7000/ravenscar-full/adalib/ $PWD/cortex-ar/zynq7000/ravenscar-full/obj/ $PWD/cortex-ar/zynq7000/ravenscar-full/user_srcs/ &&
 gprbuild -P ravenscar_full_zynq7000.gpr -j0 &&
-mkdir runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000 -p &&
+mkdir -p runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000 &&
 gprinstall -P ravenscar_full_zynq7000.gpr -p -f -XPREFIX=$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000 --install-name=ravenscar_full_zynq7000 &&
 [ -d "$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000" ] && mv $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/ravenscar_full_zynq7000/* $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/adalib/;
 [ -d "$PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000" ] && mv $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/ravenscar_full_zynq7000/* $PWD/runtime/arm-eabi/lib/gnat/ravenscar_full_zynq7000/gnarl/;
